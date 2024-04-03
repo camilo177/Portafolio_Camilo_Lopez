96efea71aaed877a1b4589ccc9c9f417495669dd
camilo177 <juanc.lopezca@campusucc.edu.co>
2024-04-03 09:46:03 -0500

    change of color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <main className="font-roboto flex justify-center items-center w-screen h-screen">
-      <h1 className="text-3xl font-bold text-red-700">Hello World</h1>
+      <h1 className="text-3xl font-bold text-green-700">Hello World</h1>
     </main>
   )
 }
