f8ad3d1dd2e94fbb5374308813e1e3ca8abd225f
camilo177 <juanc.lopezca@campusucc.edu.co>
2024-04-03 08:52:21 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="font-roboto flex justify-center items-center w-screen h-screen">
+      <h1 className="text-3xl font-bold text-red-700">Hello World</h1>
+    </main>
+  )
+}
+
+export default App
